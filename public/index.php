58f248fbfe6318fa66f5456601d555da35c197b6
Maka <maka31600@gmail.com>
2019-08-14 01:02:45 +0200

    Router

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 require '../vendor/autoload.php';
 
-$app = new \Framework\App();
+$app = new \Framework\App([
+    \App\Blog\BlogModule::class
+]);
 
 $response = $app->run(\GuzzleHttp\Psr7\ServerRequest::fromGlobals());
 \Http\Response\send($response);
